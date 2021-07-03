326c10a9b04124ee9c668bab812c18cd1c5645fe
kanuu <123kanuu@gmail.com>
2021-07-03 15:09:03 +0900

    Apply bootstrap

--- list2/script.js
+++ list2/script.js
@@ -1,25 +1,31 @@
 let dom = document.querySelector('#root')
+
 let element = React.createElement(
   'div', {}, [
     React.createElement(
       'h2', {}, "Hello!"
     ),
     React.createElement(
-      'h3', {}, "React sample page."
+      'h4', {className:'alert alert-primary'}, 
+      "React sample page."
     ),
     React.createElement(
-      'ul', {}, [
+      'ul', {className:'list-group'}, [
         React.createElement(
-          'li', {}, "First item."
+          'li', {className:'list-group-item'}, 
+          "First item."
         ),
         React.createElement(
-          'li', {}, "Second item."
+          'li', {className:'list-group-item'},
+          "Second item."
         ),
         React.createElement(
-          'li', {}, "Third item."
+          'li', {className:'list-group-item'},
+          "Third item."
         ),
       ]
     ),
   ]
 )
+
 ReactDOM.render(element, dom)
