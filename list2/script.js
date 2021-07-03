66a90b1a7ad694419fb79a4ca36b97e21baf2963
kanuu <123kanuu@gmail.com>
2021-07-03 14:55:21 +0900

    Create sample list

--- list2/script.js
+++ list2/script.js
@@ -1,5 +1,25 @@
 let dom = document.querySelector('#root')
 let element = React.createElement(
-  'p', {}, 'Hello React!'
+  'div', {}, [
+    React.createElement(
+      'h2', {}, "Hello!"
+    ),
+    React.createElement(
+      'h3', {}, "React sample page."
+    ),
+    React.createElement(
+      'ul', {}, [
+        React.createElement(
+          'li', {}, "First item."
+        ),
+        React.createElement(
+          'li', {}, "Second item."
+        ),
+        React.createElement(
+          'li', {}, "Third item."
+        ),
+      ]
+    ),
+  ]
 )
 ReactDOM.render(element, dom)
