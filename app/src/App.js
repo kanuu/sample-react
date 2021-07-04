011c1a813f5466250c5bbc0f9af2409da8ed04fe
kanuu <123kanuu@gmail.com>
2021-07-04 15:06:51 +0900

    Fix App.js

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  render(){
+  constructor(props) {
+    super()
+    this.title = props.title
+    this.message = props.message
+  }
+
+  render() {
     return  <div>
     <h1 className="bg-primary text-white display-4">React</h1>  
     <div className="container">
-      <p className="subtitle">This is sample component.</p>
-      <p>これはサンプルのコンポーネントです。</p>
-      <p>簡単なメッセージを表示します。</p>
+      <p className="subtitle">{this.title}</p>
+      <p>{this.message}</p>
     </div>
   </div>
   }
