f8506781ce655f7a07137e28147e068f9d3240bc
kanuu <123kanuu@gmail.com>
2021-07-04 16:03:31 +0900

    Clicker

--- app/src/App.js
+++ app/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: 'count start!',
+      flg: 0,
     }
 
     this.doAction = this.doAction.bind(this)
@@ -18,7 +19,8 @@ class App extends Component {
   doAction(event) {
     this.setState({
       counter: this.state.counter + 1,
-      msg: "*** count: " + this.state.counter + " ***"
+      msg: this.state.counter,
+      flg: this.state.counter % 2 === 0,
     })
   }
 
@@ -27,7 +29,15 @@ class App extends Component {
     <h1 className="bg-primary text-white display-4">React</h1>  
     <div className="container">
       <p className="subtitle">カウント数</p>
-      <p className="alert alert-warning">{this.state.msg}</p>
+      {this.state.flg ?
+        <div>
+          <p className="alert alert-warning">偶数: {this.state.msg}</p>
+        </div>
+      :
+        <div>
+          <p className="alert alert-warning">奇数：{this.state.msg}</p>
+        </div>
+      }
       <button
         className="btn btn-primary"
         onClick={this.doAction}
