c4a2add71dc1a5c3bc0e9e5af13c62a9cc083b9a
kanuu <123kanuu@gmail.com>
2021-07-04 16:48:30 +0900

    Form

--- app/src/App.js
+++ app/src/App.js
@@ -6,49 +6,44 @@ class App extends Component {
 
   constructor(props){
     super(props)
+    this.state = {
+      title: 'input form',
+      message:'type your name.'
+    }
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
   }
 
-  render() {
-    return <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
-      <div className="container">
-        <Message title="Children!">
-          これはコンポーネント内のコンテンツです。
-          マルでテキストを分割し、リストにして表示します。
-          改行は必要ありません。
-        </Message>
-      </div>
-    </div>
+  doChange(event) {
+    this.input = event.target.value;
   }
-}
 
-class Message extends Component {
-  li = {
-    fontSize: "14pt",
-    fontWeight: "bold",
-    color: "#090",
+  doSubmit(event) {
+    this.setState({
+      title: 'send form',
+      message: 'Hello, ' + this.input + '!!'
+    })
+    event.preventDefault()
   }
 
   render(){
-    let content = this.props.children
-    let arr = content.split('。')
-    let arr2 = [];
-    for(let i = 0; i < arr.length; i++){
-      if (arr[i].trim() != ''){
-        arr2.push(arr[i]);
-      }
-    }
-    let list = arr2.map((value,key) => (
-      <li className="list-group-item"
-        style={this.li} 
-        key={key}
-      >
-        {key + 1}. {value} .
-      </li>
-    ))
     return <div>
-      <h2>{this.props.title}</h2>
-      <ol className="list-group">{list}</ol>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <h4>{this.state.title}</h4>
+        <p className="card h5 p-3">{this.state.message}</p>
+        <div className="alert alert-primary mt-3">
+          <form onSubmit={this.doSubmit}>
+            <div className="form-group">
+              <label>Message:</label>
+              <input type="text" className="form-control" 
+                  onChange={this.doChange} />
+            </div>
+            <input type="submit" className="btn btn-primary" 
+                value="Click" />
+          </form>
+        </div>
+      </div>
     </div>
   }
 }
