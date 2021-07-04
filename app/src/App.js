be6fbcc3c468476ba58de58b4a8e7e7168dfb455
kanuu <123kanuu@gmail.com>
2021-07-04 16:46:50 +0900

    Add props.children

--- app/src/App.js
+++ app/src/App.js
@@ -2,72 +2,54 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  data = [
-    "This is list sample.",
-    "これはリストのサンプルです。",
-    "配列をリストに変換します。"
-  ]
+  input = ''
 
   constructor(props){
     super(props)
-    this.state = {
-      list: this.data
-    }
   }
 
   render() {
-    return  <div>
-      <h1 className="bg-primary text-white display-4">React</h1>  
+    return <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">Show List.</p>
-        <List title="サンプル・リスト" data={this.data} />
+        <Message title="Children!">
+          これはコンポーネント内のコンテンツです。
+          マルでテキストを分割し、リストにして表示します。
+          改行は必要ありません。
+        </Message>
       </div>
     </div>
   }
 }
 
-class List extends Component {
-  number = 1
-
-  render() {
-    let data = this.props.data;
-     return (
-      <div>
-       <p className="h5 text-center">{this.props.title}</p>
-        <ul className="list-group">
-          {data.map((item, key) =>
-            <li className="list-group-item" key={key}>
-              <Item number={key + 1} value={item} />
-            </li>
-          )}
-        </ul>
-      </div>
-    )
-  }
-}
-
-class Item extends Component {
-  itm = {
-    fontSize: "16pt",
-    color: "#00f",
-    textDecoration: 'underline',
-    textDecorationColor: '#ddf'
-  }
-
-  num = {
+class Message extends Component {
+  li = {
+    fontSize: "14pt",
     fontWeight: "bold",
-    color: "red"
+    color: "#090",
   }
 
-  render() {
-    return (
-      <p style={this.itm}>
-        <span style={this.num}>
-          [{this.props.number}]&nbsp;
-        </span>
-        {this.props.value}
-      </p>
-    )
+  render(){
+    let content = this.props.children
+    let arr = content.split('。')
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+      if (arr[i].trim() != ''){
+        arr2.push(arr[i]);
+      }
+    }
+    let list = arr2.map((value,key) => (
+      <li className="list-group-item"
+        style={this.li} 
+        key={key}
+      >
+        {key + 1}. {value} .
+      </li>
+    ))
+    return <div>
+      <h2>{this.props.title}</h2>
+      <ol className="list-group">{list}</ol>
+    </div>
   }
 }
 
