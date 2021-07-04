5b449938fc901dce4fde41268812613599139f28
kanuu <123kanuu@gmail.com>
2021-07-04 15:43:43 +0900

    Add count timer

--- app/src/App.js
+++ app/src/App.js
@@ -8,15 +8,23 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      msg: 'Hello component!'
+      msg: 'Hello component!',
+      count: 0,
     }
+
+    let timer = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+        msg: "[ count: " + this.state.count + " ]"
+      })
+    }, 1000)
   }
 
   render() {
     return  <div>
     <h1 className="bg-primary text-white display-4">React</h1>  
     <div className="container">
-      <p className="subtitle">{this.title}</p>
+      <p className="subtitle">カウント数</p>
       <p className="alert alert-warning">{this.state.msg}</p>
       <p className="alert alert-dark">{this.props.msg}</p>
     </div>
