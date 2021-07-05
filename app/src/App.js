c5329fb78a44c006ad3d217b471a72e3df72d883
kanuu <123kanuu@gmail.com>
2021-07-06 01:09:36 +0900

    wip

--- app/src/App.js
+++ app/src/App.js
@@ -1,47 +1,94 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 
-const useTax = (t1, t2)=> {
-  const [price, setPrice] = useState(1000)
-  const [tx1] = useState(t1)
-  const [tx2] = useState(t2)
-
-  const tax = ()=> { 
-    return Math.floor(price * (1.0 + tx1 / 100))
+// 合計計算の関数
+const total = (a) => {
+  let re = 0
+  for(let i = 0;i <= a;i++) {
+    re += i
   }
-  const reduced = ()=> { 
-    return Math.floor(price * (1.0 + tx2 / 100))
-  }
-
-  return [price, tax, reduced, setPrice]
+  return re
+}
+// 消費税計算の関数
+const tax = (a) => {
+  return Math.floor(a * 1.1)
 }
 
-function AlertMessage(props) {
-  const [price, tax, reduced, setPrice] = useTax(10, 8)
+// 数値を計算しメッセージを返す独自フック関数
+function useCalc(num = 0, func = (a) => {return a}) {
+  const [msg, setMsg] = useState(null)
+
+  const setValue = (p) => {
+    let res = func(p)
+    setMsg(<p className="h5">※ {p} の結果は、{res} です。</p>)
+  }
   
-  const DoChange = (e)=> {
-    let p = e.target.value
-    setPrice(p)
+  return [msg, setValue]
+}
+
+// デフォルトのコンポーネント
+function PlainMessage(props) {
+  const [msg, setCalc] = useCalc()
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
   }
 
-  return <div className="alert alert-primary h5">
-    <p className="h5">通常税率: {tax()} 円.</p>
-    <p className="h5">軽減税率: {reduced()} 円.</p>
-    <div className="form-group">
-      <label className="form-group-label">Price:</label>
-      <input type="number" className="form-control"
-        onChange={DoChange} value={price} />
-    </div>
+  return <div className="p-3 h5">
+    <h5>{msg}</h5>
+    <input
+      type="number"
+      onChange={onChange}
+      className="form-control"
+    />
   </div>
 }
 
+// 合計計算コンポーネント
+function AlertMessage(props) {
+  const [msg, setCalc] = useCalc(0, total)
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
+  }
+
+  return <div className="alert alert-primary h5 text-primary">
+    <h5>{msg}</h5>
+    <input
+      type="number"
+      onChange={onChange}
+      min="0"
+      max="10000"
+      className="form-control"
+    />
+  </div>
+}
+
+// 消費税計算コンポーネント
+function CardMessage(props) {
+  const [msg, setCalc] = useCalc(0, tax)
+
+  const onChange = (e)=> {
+    setCalc(e.target.value)
+  }
+
+  return <div className="card p-3 h5 border-primary">
+    <h5>{msg}</h5>
+    <input type="range" onChange={onChange} 
+        min="0" max="10000" step="100" className="form-control" />
+  </div>
+}
+
+// ベース・コンポーネント
 function App() {
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
+        <PlainMessage />
         <AlertMessage />
+        <CardMessage />
       </div>
     </div>
   )
