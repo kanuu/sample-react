21a934b6b49866435546e6b25fdfc6ea51b77eb2
kanuu <123kanuu@gmail.com>
2021-07-07 23:31:36 +0900

    Add memo component

---
+++ app/src/Memo.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import usePersist from '../Persist';
+
+import Item from './Item';
+
+function Memo(props) {
+  const [memo, setMemo] = usePersist("memo", [])
+  const [fmemo, setFmemo] = usePersist("findMemo", [])
+  const [mode, setMode] = usePersist("mode", 'default')
+
+  let data = []
+
+  switch(mode) {
+    case 'default':
+      data = memo.map((value, key) => (
+        <Item key={value.message} value={value} index={key + 1}/>
+      ))
+      setMode('default')
+      break
+    
+      case 'find':
+        data = fmemo.map((value, key) => (
+          <Item key={value.message} value={value} index={key + 1}/>
+        ))
+        setMode('default')
+        break
+
+      default:
+        data = memo.map((value, key) => (
+          <Item key={value.message} value={value} index={key + 1}/>
+        ))
+  }
+
+  return (
+    <table className="table mt-4">
+      <tbody>{data}</tbody>
+    </table>
+  )
+}
+
+export default Memo
