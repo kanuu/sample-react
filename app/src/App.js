27f538e31fe890eb3966b0836f9d7ccf9277dde9
kanuu <123kanuu@gmail.com>
2021-07-07 21:01:43 +0900

    hook

--- app/src/App.js
+++ app/src/App.js
@@ -3,25 +3,50 @@ import './App.css'
 import  usePersist  from './Persist'
 
 function AlertMessage(props) {
+  const [name, setName] = useState("")
+  const [mail, setMail] = useState("")
+  const [age, setAge] = useState(0)
+  const [mydata, setMyData] = usePersist("mydata" ,null)
+
+  const onChangeName = (e) => {
+    setName(e.target.value)
+  }
+
+  const onChangeMail = (e) => {
+    setMail(e.target.value)
+  }
+
+  const onChangeAge = (e) => {
+    setAge(e.target.value)
+  }
+
+  const onClick = (e) => {
+    const data = {
+      name: name,
+      mail: mail,
+      age, age,
+    }
+    setMyData(data)
+  }
 
   return <div className="alert alert-primary h5 text-primary">
     <h5 className="mb-4">{JSON.stringify(mydata)}</h5>
     <div className="form-group">
       <label className="h6">Name</label>
-      <input type="text"
+      <input type="text" onChange={onChangeName}
         className="form-control" />
     </div>
     <div className="form-group">
       <label className="h6">Mail</label>
-      <input type="mail"
+      <input type="mail" onChange={onChangeMail}
         className="form-control" />
     </div>
     <div className="form-group">
       <label className="h6">Age</label>
-      <input type="number"
+      <input type="number" onChange={onChangeAge}
         className="form-control" />
     </div>
-    <button
+    <button onClick={onClick}
       className="btn btn-primary">
       Save it!
     </button>
