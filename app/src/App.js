c7319f8a381e9d9f6d524944082c06be71ace61e
kanuu <123kanuu@gmail.com>
2021-07-07 20:52:08 +0900

    Add alertMessage

--- app/src/App.js
+++ app/src/App.js
@@ -1,81 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
+import  usePersist  from './Persist'
 
-// 合計計算の関数
-const total = (a) => {
-  let re = 0
-  for(let i = 0;i <= a;i++) {
-    re += i
-  }
-  return re
-}
-// 消費税計算の関数
-const tax = (a) => {
-  return Math.floor(a * 1.1)
-}
-
-// 数値を計算しメッセージを返す独自フック関数
-function useCalc(num = 0, func = (a) => {return a}) {
-  const [msg, setMsg] = useState(null)
-
-  const setValue = (p) => {
-    let res = func(p)
-    setMsg(<p className="h5">※ {p} の結果は、{res} です。</p>)
-  }
-  
-  return [msg, setValue]
-}
-
-// デフォルトのコンポーネント
-function PlainMessage(props) {
-  const [msg, setCalc] = useCalc()
-
-  const onChange = (e) => {
-    setCalc(e.target.value)
-  }
-
-  return <div className="p-3 h5">
-    <h5>{msg}</h5>
-    <input
-      type="number"
-      onChange={onChange}
-      className="form-control"
-    />
-  </div>
-}
-
-// 合計計算コンポーネント
 function AlertMessage(props) {
-  const [msg, setCalc] = useCalc(0, total)
-
-  const onChange = (e) => {
-    setCalc(e.target.value)
-  }
 
   return <div className="alert alert-primary h5 text-primary">
-    <h5>{msg}</h5>
-    <input
-      type="number"
-      onChange={onChange}
-      min="0"
-      max="10000"
-      className="form-control"
-    />
-  </div>
-}
-
-// 消費税計算コンポーネント
-function CardMessage(props) {
-  const [msg, setCalc] = useCalc(0, tax)
-
-  const onChange = (e)=> {
-    setCalc(e.target.value)
-  }
-
-  return <div className="card p-3 h5 border-primary">
-    <h5>{msg}</h5>
-    <input type="range" onChange={onChange} 
-        min="0" max="10000" step="100" className="form-control" />
+    <h5 className="mb-4">{JSON.stringify(mydata)}</h5>
+    <div className="form-group">
+      <label className="h6">Name</label>
+      <input type="text"
+        className="form-control" />
+    </div>
+    <div className="form-group">
+      <label className="h6">Mail</label>
+      <input type="mail"
+        className="form-control" />
+    </div>
+    <div className="form-group">
+      <label className="h6">Age</label>
+      <input type="number"
+        className="form-control" />
+    </div>
+    <button
+      className="btn btn-primary">
+      Save it!
+    </button>
   </div>
 }
 
@@ -86,9 +35,7 @@ function App() {
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <PlainMessage />
         <AlertMessage />
-        <CardMessage />
       </div>
     </div>
   )
