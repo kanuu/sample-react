f28890267c6640d263be0690c1b86deadd3fc9e0
kanuu <123kanuu@gmail.com>
2021-07-07 23:43:50 +0900

    Add AddForm component

--- app/src/Memo.js
+++ app/src/Memo.js
@@ -5,32 +5,31 @@ import Item from './Item';
 
 function Memo(props) {
   const [memo, setMemo] = usePersist("memo", [])
-  const [fmemo, setFmemo] = usePersist("findMemo", [])
-  const [mode, setMode] = usePersist("mode", 'default')
-
+  const [fmemo, setFMemo] = usePersist("findMemo", [])
+  const [mode, setMode] = usePersist('mode', 'default')
+  
   let data = []
 
-  switch(mode) {
+  switch (mode){
     case 'default':
-      data = memo.map((value, key) => (
-        <Item key={value.message} value={value} index={key + 1}/>
+      data = memo.map((value,key)=>(
+        <Item key={value.message} value={value} index={key + 1} />
       ))
-      setMode('default')
+      setMode('deafult')
       break
     
-      case 'find':
-        data = fmemo.map((value, key) => (
-          <Item key={value.message} value={value} index={key + 1}/>
-        ))
-        setMode('default')
-        break
+    case 'find':
+      data = fmemo.map((value,key)=>(
+        <Item key={value.message} value={value} index={key + 1}/>
+      ))
+      break
 
-      default:
-        data = memo.map((value, key) => (
-          <Item key={value.message} value={value} index={key + 1}/>
-        ))
+    default:
+      data = memo.map((value,key)=>(
+        <Item key={value.message} value={value} index={key + 1} />
+      ))
   }
-
+  
   return (
     <table className="table mt-4">
       <tbody>{data}</tbody>
